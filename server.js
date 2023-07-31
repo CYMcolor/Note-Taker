5ba86bf28321b9a52b87742b942b97f8068efd70
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-31 03:58:08 -0500

    add path for notes.html
    add more imports
    start of post request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Import stuff
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const db = require('./db/db.json');
 
 //init stuff
@@ -12,12 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-//apply middle ware
-app.use('/api', app);
-
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET req for notes
@@ -26,6 +29,26 @@ app.get('/api/notes', (req, res) => {
     res.json(`${req.method} request recieved to get notes`);
     //log to terminal
     console.info(`${req.method} request received to get notes`);
+});
+
+// POST req to add notes
+app.post('/api/notes', (req, res) => {
+    // show post request in log
+    console.info(`${req.method} request received to add a note`);
+    // init variables of note
+    const { title, text } = req.body;
+
+    // check if all note info is present
+    if ( title && text){
+        // new note to save
+        const newNote = { title, text};
+    }
+    
+    // read db json file
+    fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+
+    });
+
 });
 
 // listen to port 
