23ef9e7748f8f16f645179c82d27dc489c02296d
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-31 04:21:26 -0500

    add route when user inputs bad url to index.html

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+// GET Route for anything wrong
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 // GET req for notes
 app.get('/api/notes', (req, res) => {
