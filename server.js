cdd97d417e2ba5bed389d27e47863c0098fec86f
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-31 03:02:43 -0500

    add get notes request
    add listen function too

--- server.js
+++ server.js
@@ -14,7 +14,21 @@ app.use(express.static('public'));
 
 //apply middle ware
 app.use('/api', app);
+
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET req for notes
+app.get('/api/notes', (req, res) => {
+    //send message to client
+    res.json(`${req.method} request recieved to get notes`);
+    //log to terminal
+    console.info(`${req.method} request received to get notes`);
+});
+
+// listen to port 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
 );
